dafb055f3e5712956db1e994736dbffd8f0ce3de
dpembe2 <dpembe2@uw.edu>
2026-02-14 23:14:33 -0800

    Update homework6_Deborah_Pemberton.py

--- homework6_Deborah_Pemberton.py
+++ homework6_Deborah_Pemberton.py
@@ -1,4 +1,9 @@
 web_users = ["racerlectern", "pumpkinsmusic", "illustratesnow", "brashlethal", "princessdone"]
 new_users = ["pumpkinsmusic", "whisperedinject", "bookshelfdiorite", "princessdone", "regardingmerely"]
+for new_user in new_users:
+    if new_user in web_users:
+        print(f"Sorry, the username '{new_user}' is already taken.")
+    else:
+        print(f"The username '{new_user}' is available.")
 
 
