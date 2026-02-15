0045d0c97230a32b265ac00404c35ebfd90ba7d7
dpembe2 <dpembe2@uw.edu>
2026-02-14 23:23:12 -0800

    Update homework6_Deborah_Pemberton.py

--- homework6_Deborah_Pemberton.py
+++ homework6_Deborah_Pemberton.py
@@ -2,7 +2,7 @@ web_users = ["racerlectern", "pumpkinsmusic", "illustratesnow", "brashlethal", "
 new_users = ["pumpkinsmusic", "whisperedinject", "bookshelfdiorite", "princessdone", "regardingmerely"]
 for new_users in new_users:
     if new_users in web_users:
-        print(f"This user name is already in use.'{new_users}' Please choose a different username.")
+        print(f"This user name '{new_users}' is already in use. Please choose a different username.")
     else:
         print(f"The username '{new_users}' is available.")
 
