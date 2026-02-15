6bf3253a9e0e63d6dba4c710c9cd9256d38b2502
dpembe2 <dpembe2@uw.edu>
2026-02-14 23:33:25 -0800

    Update homework6_Deborah_Pemberton.py

--- homework6_Deborah_Pemberton.py
+++ homework6_Deborah_Pemberton.py
@@ -5,5 +5,22 @@ for new_users in new_users:
         print(f"This user name '{new_users}' is already in use. Please choose a different username.")
     else:
         print(f"The username '{new_users}' is available.")
+cities = {
+    'Seattle': {
+        'country': 'United States',
+        'population': '816600',
+        'fact': 'Seattle is home to Amazon and Starbucks.'
+    },
+    'London': {
+        'country': 'United Kingdom',
+        'population': '9100000',
+        'fact': 'London was founded by the Romans in AD 47.'
+    },
+    'Rome': {
+        'country': 'Italy',
+        'population': '2750000',
+        'fact': 'Rome was the first city in the world to reach a population of 1 million people.'
+    }
+}        
 
 
